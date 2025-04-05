702a2ca259bfe5a02b677eb5574eec20dfa93183
Tais <taisdias76@gmail.com>
2025-04-05 20:58:24 -0300

    INV-5: Controller de produtos

---
+++ src/main/java/com/tais/inventario/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.tais.inventario.controller;
+
+import com.tais.inventario.dtos.ProductDTO;
+import com.tais.inventario.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    private final ProductService service;
+
+    @Autowired
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductDTO> create(@RequestBody ProductDTO request) {
+        ProductDTO dto = service.create(request);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductDTO>> getAll() {
+        List<ProductDTO> productDTOS = service.getAll();
+        return ResponseEntity.ok(productDTOS);
+    }
+}
--- src/main/java/com/tais/inventario/entities/Product.java
+++ src/main/java/com/tais/inventario/entities/Product.java
@@ -1,13 +1,9 @@
 package com.tais.inventario.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Setter
-@Getter
 @Entity
 public class Product {
     @Id
@@ -30,4 +26,59 @@ public class Product {
 
     private BigDecimal quantity;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
